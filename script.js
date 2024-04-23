e64781ae3ad0f50feefac2475ca35e73814f5f5a
basiaz79 <bzasowski79@gmail.com>
2024-04-23 21:49:08 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Zadanie 1: Tworzenie Tablic
+const fruits = ["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"];
+
+// Zadanie 2: Iteracja po Tablicach
+var numbers = [1, 4, 6, 8, 3, 5, 7, 9]; 
+
+//Pętla iterująca po tablicy numbers i wyświetlająca liczby większe niż 5
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 5) {
+    console.log(numbers[i]);
+  }
+}
